d97a4bd430812bd1e8b28b19e5ffd4949761b817
burak <burak@pau.edu.tr>
2024-06-12 13:08:16 +0300

    1st commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+﻿document.getElementById('call-taxi').addEventListener('click', function () {
+    alert('Taksi çağırma işlemi başlatıldı.');
+});
+
+// Harita işlevselliği için Google Maps API veya başka bir harita API'si kullanabilirsiniz
+function initMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(37.7749, -122.4194),
+        zoom: 10,
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+// Harita API'si çağrısı
+function loadScript() {
+    var script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
+    document.body.appendChild(script);
+}
+
+window.onload = loadScript;
